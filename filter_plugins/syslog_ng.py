b11445e994e68447af7f0e10fbc7e02b21d6b495
Bodo Schulz <bodo@boone-schulz.de>
2022-12-06 04:33:00 +0100

    changes for remote log server

--- filter_plugins/syslog_ng.py
+++ filter_plugins/syslog_ng.py
@@ -1,5 +1,7 @@
 # python 3 headers, required if submitting to Ansible
 from __future__ import (absolute_import, division, print_function)
+
+from ansible.utils.color import stringc
 from ansible.utils.display import Display
 
 import re
@@ -8,7 +10,6 @@ import os
 __metaclass__ = type
 
 display = Display()
-
 
 class FilterModule(object):
     """
@@ -20,23 +21,25 @@ class FilterModule(object):
             'type': self.var_type,
             'get_service': self.get_service,
             'log_directories': self.log_directories,
+            'check_obsolete_config': self.check_obsolete_config,
+            'migrate_config': self.migrate_config,
+            'warning' : self.warning,
+            'deprecated': self.deprecated,
+            'template_syslog': self.template_syslog
         }
 
     def var_type(self, var):
-        '''
-        Get the type of a variable
-        '''
-        # display.v("var   : {} ({})".format(var, type(var)))
-        # display.v("result: {}".format(type(var).__name__))
-
+        """
+          Get the type of a variable
+        """
         _type = type(var).__name__
 
-        display.v(f" {var}, type: {_type}")
+        # display.v(f" {var}, type: {_type}")
 
-        if (isinstance(var, str) or _type == "AnsibleUnsafeText"):
+        if isinstance(var, str) or _type == "AnsibleUnsafeText":
             _type = "str"
 
-        display.v(f" = result {_type}")
+        # display.v(f" = result {_type}")
 
         return _type
 
@@ -44,12 +47,11 @@ class FilterModule(object):
         name = None
         # count = len(data.keys())
         # display.vv("found: {} entries, use filter {}".format(count, search_for))
-        regex_list_compiled = re.compile("^{}.*.service$".format(search_for))
+        regex_list_compiled = re.compile(f"^{search_for}.*.service$")
 
         for k, v in data.items():
-            display.v("  - {}  - {}".format(k, v))
+            display.v(f"  - {k}  - {v}")
             if(re.match(regex_list_compiled, k)):
-                # display.v("  = {}  - {}".format(k, v))
                 name = v.get('name')
                 break
 
@@ -78,5 +80,97 @@ class FilterModule(object):
         # remove base_directory
         _ = unique_dirs.remove(base_directory)
 
-        display.v(" = result {}".format(unique_dirs))
+        display.v(f" = result {unique_dirs}")
         return unique_dirs
+
+    def check_obsolete_config(self, data):
+        """
+        """
+        result = [k for k, v in data.items() if v.get("file_name", None)]
+        # okay = [k for k, v in data.items() if not v.get("file_name", None)]
+#         if len(result) > 0:
+#             """
+#             """
+#             m = ", ".join(result)
+#             msg = f"""
+# The following "syslog_logs" configurations have an old syntax and should be migrated:
+#   - {m}
+# """
+#
+#             result = msg
+
+        return result
+
+    def migrate_config(self, data, dest, sources={}):
+        """
+        """
+        # display.v(f"data: {data}, {dest}, sources")
+
+        result = {}
+
+        if dest == "syslog_destinations":
+            """
+            """
+            _data = self.__merge_two_dicts(data, sources)
+        else:
+            _data = data.copy()
+
+        for k, v in _data.items():
+            """
+            """
+            if dest == "syslog_logs" and v.get("file_name", None):
+                _ = v.pop("file_name")
+
+            if dest == "syslog_destinations":
+                if v.get("filter", None):
+                    _ = v.pop("filter")
+
+        return _data
+
+
+    def warning(self, data, columns, hostname, version):
+        """
+        """
+        display.columns = int(columns)
+
+        msg = f"""
+{hostname}: {data}
+This feature will be removed in version {version}"""
+
+        return display.warning(msg, formatted = True)
+
+    def deprecated(self, data, version):
+        """
+        """
+        display.columns = 95
+        return display.deprecated(data, version=version)
+
+
+    def __merge_two_dicts(self, x, y):
+        z = x.copy()   # start with x's keys and values
+        z.update(y)    # modifies z with y's keys and values & returns None
+        return z
+
+    def template_syslog(self, data):
+        """
+        """
+        display.v(f"template_syslog({data})")
+
+        _transport = data.get("transport", None)
+        _port = data.get("port", None)
+
+        if not _transport or not _transport in ["udp", "tcp", "tls"]:
+            _transport = "udp"
+
+        if _transport == "udp" and not _port:
+            _port = 514
+
+        if _transport == "tcp" and not _port:
+            _port = 601
+
+        if _transport == "tls" and not _port:
+            _port = 6514
+
+        return _transport, _port
+
+
